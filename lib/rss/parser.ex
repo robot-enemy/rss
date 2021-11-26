38909f59f67fe7b3532a84360f28d3389bf51fcc
Matthew Slater <delameko@delameko-stone.com>
2021-11-26 12:38:55 +0000

    Improvements to the xml parser

--- lib/rss/parser.ex
+++ lib/rss/parser.ex
@@ -1,5 +1,14 @@
 defmodule RSS.Parser do
-  @moduledoc false
+  @moduledoc """
+  This was a quick attempt to create an parser, but ultimately reverted to a
+  third party library.
+
+  I'm leaving it here in case I ever need to come back to it.
+
+  Things that it needs: a Tree module that would traverse the nodes, there's
+  potential errors here where returning the wrong nodes could mess up the tree;
+  dealing with categories; general cleanup.
+  """
 
   def parse(path) do
     initial_state = {{}, nil}
@@ -9,65 +18,52 @@ defmodule RSS.Parser do
     ]
 
     case :xmerl_sax_parser.file(path, opts) do
-      {:ok, {_initial_state, state}, _} -> {:ok, state} |> IO.inspect()
+      {:ok, {_initial_state, state}, _} -> {:ok, state}
       error -> error
     end
   end
 
-  # def event(:startDocument, _, state) do
-  #   IO.puts "Begin"
-  #   state
-  # end
+  defp extract_attr(html_attrs, key) do
+    {_, _, _name, value} = Enum.find(html_attrs, fn {_, _, name, _} -> name == key end)
+    to_string(value)
+  end
+
+  defp find_item_node({stack, %{"item" => _} = node}), do: {stack, node}
+  defp find_item_node({nil, _node}), do: nil
+  defp find_item_node({stack, _node}), do: find_item_node(stack)
 
   defp get_tag(node), do: node |> Map.keys() |> List.first()
 
   defp tag_name({prefix, tag}) when prefix == [], do: to_string(tag)
   defp tag_name({prefix, tag}), do: Enum.join([prefix, tag], ":")
 
-  #
-  # Item
-  #
-  # when item closes, add it to the "items" list
-  #
-  def event({:endElement, _, 'item', _}, _loc, {{stack, parent_node}, current_node}) do
-    items = parent_node["channel"]["items"] || []
-    items = items ++ [current_node]
-    parent_node = put_in(parent_node, ["channel", "items"], items)
+  ################
+  #              #
+  # startElement #
+  #              #
+  ################
 
-    {stack, parent_node}
+  def event(
+    {:startElement, _meta_terms, _tag, {'media', 'content'}, html_attrs},
+    {_path, _file, _line_num},
+    stack
+  ) do
+    {stack, %{"content" => extract_attr(html_attrs, 'url')}}
   end
 
-  #
-  # Item Media
-  #
-
-  # defp extract_attr(html_attrs, key) do
-  #   {_, _, _name, value} = Enum.find(html_attrs, fn {_, _, name, _} -> name == key end)
-  #   to_string(value)
-  # end
-
-  # def event(
-  #   {:startElement, _meta_terms, _tag, {'media', 'thumbnail'}, html_attrs},
-  #   {_path, _file, _line_num},
-  #   stack
-  # ) do
-  #   if get_tag(current_node) == "media" do
-  #     {stack, Map.mergecurrent_node}, %{"thumbnail" => extract_attr(html_attrs, 'url')}}
-  #   else
-  #     {{stack, current_node}, %{"media"}}
-  # end
-
-  #
-  # General
-  #
+  def event(
+    {:startElement, _meta_terms, _tag, {'media', 'thumbnail'}, html_attrs},
+    {_path, _file, _line_num},
+    stack
+  ) do
+    {stack, %{"thumbnail" => extract_attr(html_attrs, 'url')}}
+  end
 
   def event(
     {:startElement, _meta_terms, _tag, {_prefix, tag}, _html_attrs},
     {_path, _file, _line_num},
     stack
   ) do
-    # tag = tag_name(tag_with_prefix)
-
     {stack, %{to_string(tag) => nil}}
   end
 
@@ -79,18 +75,49 @@ defmodule RSS.Parser do
     {stack, Map.put(current_node, key, content)}
   end
 
+  ##############
+  #            #
+  # endElement #
+  #            #
+  ##############
+
+  #
+  # when item closes, add it to the "items" list
+  #
+  def event({:endElement, _, 'item', _}, _loc, {{stack, parent_node}, current_node}) do
+    items = parent_node["channel"]["items"] || []
+    items = items ++ [current_node]
+    parent_node = put_in(parent_node, ["channel", "items"], items)
+
+    {stack, parent_node}
+  end
+
+  # This is a patch for the media element, as they function differently than
+  # other elements.
+  def event(
+    {:endElement, _meta_terms, _tag, {'media', _tag} = el},
+    {_path, _file_, _line_num},
+    {stack, current_node}
+  ) do
+    if current_node["item"] do
+      {stack, current_node}
+    else
+      {stack, item_node} = find_item_node({stack, current_node})
+
+      if media = item_node["item"]["media"] do
+        {stack, put_in(item_node, ["item", "media"], Map.merge(media, current_node))}
+      else
+        {stack, put_in(item_node, ["item", "media"], current_node)}
+      end
+    end
+  end
+
   #
   # If the parent element is nil, we're done.  Return the stack
   #
   def event({:endElement, _, _, _}, _, {{stack, nil}, current_node}) do
     {stack, current_node}
   end
-
-  ##############
-  #            #
-  # endElement #
-  #            #
-  ##############
 
   def event(
     {:endElement, _meta_terms, _tag, {[], _tag}},
@@ -102,32 +129,6 @@ defmodule RSS.Parser do
     parent_node = Map.put(parent_node, parent_tag, parent_content)
 
     {stack, parent_node}
-  end
-
-  # This is a patch for the media element, as they function differently than
-  # other elements.
-  def event(
-    {:endElement, _meta_terms, _tag, {'media', _tag} = el},
-    {_path, _file_, _line_num},
-    {{{stack, grandparent_node}, parent_node}, current_node}
-  ) do
-    cond do
-      current_node["media"] ->
-        # We're currently in the "item", so just return the stack
-        {{{stack, grandparent_node}, parent_node}, current_node}
-      media = parent_node["media"] ->
-        # The parent node is the "item", so update the "media" value
-        media = Map.merge(media, current_node)
-        {{stack, grandparent_node}, Map.put(parent_node, "media", media)}
-      media = grandparent_node["media"] ->
-        # We're two media deep, the "item" is the grandparent, so we want to
-        # merge up
-        media = Map.merge(media, current_node)
-        {stack, Map.put(grandparent_node, "media", media)}
-      true ->
-        # Else we merge a new "media" into the parent
-        {{stack, grandparent_node}, Map.put(parent_node, "media", current_node)}
-    end
   end
 
   def event(
@@ -147,8 +148,6 @@ defmodule RSS.Parser do
 
     {stack, parent_node}
   end
-
-
 
   def event(:endDocument, _, stack), do: stack
 
